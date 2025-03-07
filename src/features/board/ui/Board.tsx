1cb2feb50638b52f20c6e14377279ff33e4c9e51
Rafaelkocharli <Rafaelkocharli@gmail.com>
2025-03-08 02:18:44 +0300

    add board styles

--- src/features/board/ui/Board.tsx
+++ src/features/board/ui/Board.tsx
@@ -1,16 +1,22 @@
-import { useSelector} from "react-redux";
-import {RootState} from "../../../app/store";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import styles from "./Board.module.scss";
 
 export default function Board() {
+  // const dispatch = useDispatch()
+  const board = useSelector((state: RootState) => state.board);
 
-    // const dispatch = useDispatch()
-    const board = useSelector((state: RootState) => state.board)
-
-    return (
-        <div>
-            <table>
-                {board.map((row) => <tr>{row.map((col) => <td>{col}</td>)}</tr>)}
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table className={styles.board}>
+        {board.map((row) => (
+          <tr>
+            {row.map((col) => (
+              <td>{col}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
