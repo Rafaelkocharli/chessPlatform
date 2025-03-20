2882019558c9aa76d87efaae2def7f5fd3b2e6b4
Rafael Kocharli <rafaelkocharli@gmail.com>
2025-03-20 15:42:29 +0300

    fix

--- src/processes/game/__tests__/controller.test.ts
+++ src/processes/game/__tests__/controller.test.ts
@@ -14,6 +14,7 @@ describe('Controller module tests', () => {
         ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',],
     ]
 
+
     const whiteFactory = new WhiteFactory();
 
     test("initBoardFromMatrix function test", () => {
